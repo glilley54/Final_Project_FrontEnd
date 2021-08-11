8f76c6937c8e5291ed9c40ed41847a3a9c86e6ec
glilley54 <glilley54@gmail.com>
2021-08-11 15:55:35 +0100

    more styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,16 @@ import { Container, Image, Row, Col} from "react-bootstrap";
 const Home = () => (
   <>
   <Container> 
+
   <br></br>
 
     <header>
-    <h1>Welcome Dough Delivered!</h1>
-    </header>  
+    <h1 class="text-center" >Welcome Dough Delivered!</h1>
+    </header> 
+
+    <br></br> 
+
+ 
     
     <Row>
     <Col>
@@ -17,7 +22,7 @@ const Home = () => (
    </Col>
 
    <Col> 
-    <p>Our dough balls are made with the finest quality ingredients: Caputo Tipo 00 Pizzeria flour, live yeast, purified water and Himalayan pink salt. Our dough balls are made to our own 60% hydration recipe, and each dough ball weighs 250 grams, perfect for a 12" pizza.
+    <p class="text-justify center" class="text-secondary">Our dough balls are made with the finest quality ingredients: Caputo Tipo 00 Pizzeria flour, live yeast, purified water and Himalayan pink salt. Our dough balls are made to our own 60% hydration recipe, and each dough ball weighs 250 grams, perfect for a 12" pizza.
 
 Dough balls will be semi-frozen on arrival, and can safely be refrozen without affecting the final result (amazing pizza!).
 
@@ -26,7 +31,8 @@ Once shipped dough balls will be delivered via a specialist 24-hour courier, so 
 Each package of dough balls comes with complete defrosting and preparation instructions. The minimum defrosting time required is one hour, but they can be left in the fridge overnight too.
 
 Please note: Your dough balls will be shipped immediately, as per delivery schedule below, even if your order includes other products (for example, ovens on pre-order!). If you’d like to use your dough balls with a new oven, we recommend you wait until you receive your oven before placing an order.</p>
-<p>Our dough balls are made with the finest quality ingredients: Caputo Tipo 00 Pizzeria flour, live yeast, purified water and Himalayan pink salt. Our dough balls are made to our own 60% hydration recipe, and each dough ball weighs 250 grams, perfect for a 12" pizza.
+
+<p class="text-justify center" class="text-secondary">Our dough balls are made with the finest quality ingredients: Caputo Tipo 00 Pizzeria flour, live yeast, purified water and Himalayan pink salt. Our dough balls are made to our own 60% hydration recipe, and each dough ball weighs 250 grams, perfect for a 12" pizza.
 
 Dough balls will be semi-frozen on arrival, and can safely be refrozen without affecting the final result (amazing pizza!).
 
@@ -35,8 +41,8 @@ Once shipped dough balls will be delivered via a specialist 24-hour courier, so 
 Each package of dough balls comes with complete defrosting and preparation instructions. The minimum defrosting time required is one hour, but they can be left in the fridge overnight too.
 
 Please note: Your dough balls will be shipped immediately, as per delivery schedule below, even if your order includes </p>
-
-<p>Our dough balls are made with the finest quality ingredients: Caputo Tipo 00 Pizzeria flour, live yeast, purified water and Himalayan pink salt. Our dough balls are made to our own 60% hydration recipe, and each dough ball weighs 250 grams, perfect for a 12" pizza.
+<br></br>
+<p class="text-justify center" class="text-secondary">Our dough balls are made with the finest quality ingredients: Caputo Tipo 00 Pizzeria flour, live yeast, purified water and Himalayan pink salt. Our dough balls are made to our own 60% hydration recipe, and each dough ball weighs 250 grams, perfect for a 12" pizza.
 
 Dough balls will be semi-frozen on arrival, and can safely be refrozen without affecting the final result (amazing pizza!).
 
@@ -47,6 +53,8 @@ Each package of dough balls comes with complete defrosting and preparation instr
 Please note: Your dough balls will be shipped immediately, as per delivery schedule below, </p>
 </Col>
   </Row>
+
+ 
   
   </Container>
   </>
