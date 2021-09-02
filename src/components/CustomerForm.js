c5b8d5daed3c149868d7e0925b53b7c3dd9e57a9
glilley54 <glilley54@gmail.com>
2021-09-02 14:20:39 +0100

    about to amend form on Customer form file

--- src/components/CustomerForm.js
+++ src/components/CustomerForm.js
@@ -2,7 +2,8 @@ import {useState} from "react";
 import {postCustomer} from "./CustomerService";
 import { postOrder } from "./OrderService";
 import { useHistory } from "react-router-dom";
-import { Button, Container, Row,Image, CardColumns, Card} from "react-bootstrap";
+import { Button, Carousel,Container, Row,Image, CardColumns, Card} from "react-bootstrap";
+
 
 const CustomerForm = ({basket}) => {
 
@@ -28,10 +29,10 @@ const CustomerForm = ({basket}) => {
     return (
 
         <>
+        <h1>Please Complete your order...</h1>
         <Container
-
-    
-        className="m-6" style={{ width: "20rem" }}>
+        
+        className="m-5" style={{ width: "20rem" }}>
         <form onSubmit={onSubmit} id="customer-form" >
             <h2>Customer Details</h2>
             <div className="form-control">
@@ -59,9 +60,36 @@ const CustomerForm = ({basket}) => {
             <Button  onClick={onSubmit}>Confirm Order</Button>
 	    </form>
         
-        <Image src="https://images.unsplash.com/photo-1583729101899-55bb892b917c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80" fluid />
         
         </Container> 
+
+        <Container>
+
+    <Carousel align="center">
+  <Carousel.Item interval={2000}>
+
+    <img width={1300} height={500} alt="1000x500"src="https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+      alt="First slide"
+    />
+  
+  </Carousel.Item>
+
+  <Carousel.Item interval={2000}>
+    <img width={1300} height={500} alt="1000x500"src="https://images.unsplash.com/photo-1534963063257-6b65b54e438b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80"
+      alt="Second slide"
+    />
+  </Carousel.Item>
+  <Carousel.Item interval={2000}>
+    <img width={1300} height={500} alt="1000x500"
+      src="https://images.unsplash.com/photo-1564128442383-9201fcc740eb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1402&q=80"
+      alt="Third slide"
+    />
+  </Carousel.Item>
+</Carousel>
+
+    <br></br>
+
+    </Container>
 
         </>
     
